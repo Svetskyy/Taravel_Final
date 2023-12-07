f84ea848cdff09f4285cfca2fbcf10ae248c83e6
Svetskyy <84508480+Svetskyy@users.noreply.github.com>
2023-12-07 22:06:42 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -2,42 +2,39 @@ const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const zlib = require("zlib");
+const NodeCache = require("node-cache");
 
+const cache = new NodeCache({ stdTTL: 3600 }); // Set expiration time to 1 hour
 
 const db = mysql.createPool({
-  connectionLimit: 10,
+  connectionLimit: 100,
   host: "154.41.240.230",
   user: "u532639681_root",
   password: "W@2915djkq#",
   database: "u532639681_mydatabase",
   compress: true, // Enable compression
   stream: function (options, callback) {
-    // Use zlib.createDeflateRaw() for raw deflate compression
     return zlib.createGzip(options, callback);
   },
 });
 
-// The pool will emit a connection event when a new connection is made
 db.on('connection', (connection) => {
   console.log('New connection made to the database');
 });
 
-// The pool will emit an error event if a connection cannot be made
 db.on('error', (err) => {
   console.error('Error in MySQL connection pool:', err);
 });
+
 const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
 
-// Serve static files from the root directory
 app.use(express.static("./"));
 
-// Optional: Default route to serve index.html
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: "." });
 });
 
-// Check database connection status
 app.get("/api/check-connection", (req, res) => {
   if (db.state === 'disconnected') {
     res.json({ connected: false });
@@ -48,22 +45,32 @@ app.get("/api/check-connection", (req, res) => {
 
 app.post("/api/check", (req, res) => {
   const { sourceCoordinates, destCoordinates } = req.body;
-  db.query(
-    "SELECT algResults FROM genetic_data1 WHERE sourceCoordinates = ? AND destCoordinates = ?",
-    [JSON.stringify(sourceCoordinates), JSON.stringify(destCoordinates)],
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Database error");
-        return;
+
+  const cacheKey = `${JSON.stringify(sourceCoordinates)}_${JSON.stringify(destCoordinates)}`;
+  const cachedResult = cache.get(cacheKey);
+  if (cachedResult) {
+    const algResultsObject = JSON.parse(cachedResult);
+    res.json({ exists: true, algResults: algResultsObject });
+  } else {
+    db.query(
+      "SELECT algResults FROM genetic_data1 WHERE sourceCoordinates = ? AND destCoordinates = ?",
+      [JSON.stringify(sourceCoordinates), JSON.stringify(destCoordinates)],
+      (err, results) => {
+        if (err) {
+          res.status(500).send("Database error");
+          return;
+        }
+
+        if (results.length > 0) {
+          const algResultsObject = JSON.parse(results[0].algResults);
+          cache.set(cacheKey, JSON.stringify(algResultsObject));
+          res.json({ exists: true, algResults: algResultsObject });
+        } else {
+          res.json({ exists: false });
+        }
       }
-      if (results.length > 0) {
-        const algResultsObject = JSON.parse(results[0].algResults);
-        res.json({ exists: true, algResults: algResultsObject });
-      } else {
-        res.json({ exists: false });
-      }
-    }
-  );
+    );
+  }
 });
 
 app.post("/api/save-result", (req, res) => {
@@ -81,6 +88,14 @@ app.post("/api/save-result", (req, res) => {
         res.status(500).send("Error saving to database");
         return;
       }
+
+      const cacheKey = `${JSON.stringify(sourceCoordinates)}_${JSON.stringify(destCoordinates)}`;
+      cache.set(cacheKey, JSON.stringify(algResults));
+
+      // Log cache content after saving
+      const cacheStats = cache.getStats();
+      console.log("Cache Stats after saving:", cacheStats);
+
       res.send({ message: "Data saved successfully", id: results.insertId });
     }
   );
@@ -89,23 +104,24 @@ app.post("/api/save-result", (req, res) => {
 
 app.delete("/api/delete-directions", (req, res) => {
   const { sourceCoordinates, destCoordinates } = req.body;
+  const cacheKey = `${JSON.stringify(sourceCoordinates)}_${JSON.stringify(destCoordinates)}`;
+  
   db.query(
     "DELETE FROM genetic_data1 WHERE sourceCoordinates = ? AND destCoordinates = ?",
-    [
-      JSON.stringify(sourceCoordinates),
-      JSON.stringify(destCoordinates),
-    ],
+    [JSON.stringify(sourceCoordinates), JSON.stringify(destCoordinates)],
     function (err) {
       if (err) {
         console.error(err);
         res.status(500).send("Error deleting directions from the database");
         return;
       }
+      
+      // Remove from cache upon deletion
+      cache.del(cacheKey);
+      
       res.status(200).send({ message: "Data deleted successfully" });
     }
   );
 });
-
-
 
 app.listen(3000, () => console.log("Server running on port 3000"));
